5c2056df9a3e02a579be2b54da1dc6af5c2379df
zlatin.ivanov <zlatin.ivanov@just-eat.com>
2013-10-15 13:25:04 +0100

    Making changes as per tutorial

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,6 +5,7 @@ angular.module('SitePointAngularAppApp')
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
-      'Karma'
+      'Karma',
+      'SitePoint'
     ];
   });
